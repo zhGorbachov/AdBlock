6b530fab911d8de0827d4681478ebc596320d7f8
Evgeniy Gorbachov <zekagor@gmail.com>
2023-09-23 17:02:47 +0300

    Extension of proxymiddleware adds to application.

--- AdBlock/AdBlock/Program.cs
+++ AdBlock/AdBlock/Program.cs
@@ -1,3 +1,5 @@
+using AdBlock.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +23,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.UseProxy();
 
 app.Run();
