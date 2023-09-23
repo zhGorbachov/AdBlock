6e96c9918348e33e4e419eabf524c5efbdfa9565
Evgeniy Gorbachov <zekagor@gmail.com>
2023-09-23 17:00:44 +0300

    Added extension to proxymiddleware.

---
+++ AdBlock/AdBlock/Extensions/ProxyMiddlewareExtension.cs
@@ -0,0 +1,12 @@
+﻿using AdBlock.Classes;
+using Microsoft.AspNetCore.Builder;
+
+namespace AdBlock.Extensions;
+
+public static class ProxyMiddlewareExtension
+{
+    public static IApplicationBuilder UseProxy(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ProxyMiddleware>();
+    }
+}
