167288f87d19af996bbec0ec12e57ccf254afd4f
Evgeniy Gorbachov <zekagor@gmail.com>
2023-09-23 16:57:43 +0300

    Added for future logger of blocked resources on the page.

---
+++ AdBlock/AdBlock/Classes/Logger.cs
@@ -0,0 +1,16 @@
+﻿namespace AdBlock.Classes;
+
+public class Logger
+{
+    private readonly ILogger<Logger> _logger;
+
+    public Logger(ILogger<Logger> logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogBlockedAdResource(string url)
+    {
+        _logger.LogInformation($"Blocked ad resource: {url}");
+    }
+}
