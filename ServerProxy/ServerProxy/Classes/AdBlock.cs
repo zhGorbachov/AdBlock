6b0a46e7468fee76f7a71337c891f93e1c0d0bff
Evgeniy Gorbachov <zekagor@gmail.com>
2023-10-04 00:41:48 +0300

    Added new functionality to block ads by using list, but now it function doesnt use.

--- ServerProxy/ServerProxy/Classes/AdBlock.cs
+++ ServerProxy/ServerProxy/Classes/AdBlock.cs
@@ -9,7 +9,7 @@ public static class AdBlock
         // Implement your ad-blocking logic here.
         // You can use regular expressions or other methods to match ad-related URLs.
         // For demonstration, we block requests containing "example.com/ad" in the URL.
-        return Regex.IsMatch(url, @"example\.com/ad", RegexOptions.IgnoreCase);
+        return Regex.IsMatch(url, @"/ad(s|verts?|vertising)?[-._]?banner(s?)|pop[-._]?up(s?)|tracker/", RegexOptions.IgnoreCase);
     }
     
     public static bool ContainsAds(string responseBody)
@@ -18,15 +18,35 @@ public static class AdBlock
         // You can use regular expressions, keywords, or other methods to detect ads
 
         // Example: Block ads based on a keyword
-        if (responseBody.Contains("advertising"))
+        
+        var filterListContentEn = File.ReadAllText("../../../Filter/list2.txt");
+        var filterListContentRu = File.ReadAllText("../../../Filter/list3.txt");
+        
+        var filtersEn = filterListContentEn.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        var filtersRu = filterListContentRu.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        var filters = filtersEn.Concat(filtersRu);
+        
+        foreach(var filter in filters)
         {
-            return true;
+            if (responseBody.Contains(filter))
+            {
+                Console.WriteLine(filter);
+                Console.WriteLine("Ban ad");
+                return true;
+            }
         }
+        
+        //
+        // if (responseBody.Contains("advertising"))
+        // {
+        //     return true;
+        // }
 
         // Example: Block ads using a regular expression
         // Replace this with a regex pattern that matches ad-related content
-        // if (Regex.IsMatch(responseBody, "your-regex-pattern"))
+        // if (Regex.IsMatch(responseBody, "/\\/(ads?|banners?|trackers?|affiliates?)\\//"))
         // {
+        //     Console.WriteLine("Ban ad");
         //     return true;
         // }
 
