83d84dd7fbc230337b322a40943c83ef0432871e
Evgeniy Gorbachov <zekagor@gmail.com>
2023-10-06 13:17:38 +0300

    Added new class where would be functionality connected with logs while you move by site-pages.

---
+++ ServerProxy/ServerProxy/Classes/Logger.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerProxy.Classes;
+
+public static class Logger
+{
+    
+    private static Dictionary<string, DateTime> lastLoggedDomains = new Dictionary<string, DateTime>();
+    private static TimeSpan logInterval = TimeSpan.FromMinutes(5);
+    
+    public static void LogBlockedAd(string domain)
+    {
+        if (!lastLoggedDomains.ContainsKey(domain) || DateTime.UtcNow - lastLoggedDomains[domain] > logInterval)
+        {
+            Console.WriteLine($"[{DateTime.Now}] Blocked ad on the {domain}");
+            lastLoggedDomains[domain] = DateTime.UtcNow;
+        }
+    }
+}
