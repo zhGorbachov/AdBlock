0c168980c02c1264c8a38b116db9329b189acd75
Evgeniy Gorbachov <zekagor@gmail.com>
2023-10-07 17:57:18 +0300

    Added method which write the last one Hostname of page.

--- ServerProxy/ServerProxy/Classes/Logger.cs
+++ ServerProxy/ServerProxy/Classes/Logger.cs
@@ -10,8 +10,26 @@ public static class Logger
     {
         if (!lastLoggedDomains.ContainsKey(domain) || DateTime.UtcNow - lastLoggedDomains[domain] > logInterval)
         {
-            Console.WriteLine($"[{DateTime.Now}] Blocked ad on the {domain}");
+            Console.WriteLine($"[ADBLOCK][{DateTime.Now}] Blocked ad on the {domain}\n");
             lastLoggedDomains[domain] = DateTime.UtcNow;
         }
+    }
+
+    public static void LogHostnamePage(string hostname)
+    {
+        switch (hostname.Split(".").Length)
+        {
+            case 2:
+                hostname = hostname.Split(".")[0];
+                break;
+            case 3:
+                hostname = hostname.Split(".")[1];
+                break;
+            case 4:
+                hostname = hostname.Split(".")[2];
+                break;
+        };
+        
+        Console.WriteLine($"[INFO][{DateTime.Now}] Proxy-server is sitting on: {hostname}");
     }
 }
